9e43a5497c39074fb6dcef02f74a46c6ad8bd865
shlee <noia1223@gmail.com>
2025-10-17 20:56:10 +0900

    .

--- src/main/java/com/crepen/crepenboard/api/common/system/model/BaseResponse.java
+++ src/main/java/com/crepen/crepenboard/api/common/system/model/BaseResponse.java
@@ -1,5 +1,7 @@
 package com.crepen.crepenboard.api.common.system.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +12,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BaseResponse implements Serializable {
     private boolean success;
     private Object data;
